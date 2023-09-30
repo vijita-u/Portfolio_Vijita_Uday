ea95740b66ce8ca83264f3a75a8f5758867e6d98
vijita-u <udayvijita3009@gmail.com>
2023-09-30 10:53:48 +0530

    Github Pages 404 issue on single page applications

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
-// import { BrowserRouter as Router } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.scss";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
