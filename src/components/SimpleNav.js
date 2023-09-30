ca0bffe21c703b109a8836769320d8fd99cddca1
vijita-u <udayvijita3009@gmail.com>
2023-09-30 11:14:55 +0530

    Fixing minor bugs

--- src/components/SimpleNav.js
+++ src/components/SimpleNav.js
@@ -15,6 +15,7 @@ function SimpleNav({ link }) {
 				className="simpleNav"
 				initial={{ opacity: 0, y: -100 }}
 				animate={{ opacity: 1, y: 0 }}
+				exit={{ opacity: 0 }}
 				transition={{
 					duration: 0.6,
 					type: "bounce",
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { HashRouter as Router } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { HashRouter as Router } from "react-router-dom";
 import "./index.scss";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
